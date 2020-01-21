a71ed7649451dc6849d5a2247c5f40a2a1c971e6
pane99 <pane99@live.com>
2020-01-20 19:18:24 -0500

    Updates

---
+++ precache-manifest.c2e743590173e6218a5e62bccb8c4c49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9528065906a3c4611094f125f5b9e749",
+    "url": "/post-it/index.html"
+  },
+  {
+    "revision": "33eff7a2fa62841300c8",
+    "url": "/post-it/static/js/2.e1c251d3.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/post-it/static/js/2.e1c251d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee6cc8c42eec431242c4",
+    "url": "/post-it/static/js/main.413080be.chunk.js"
+  },
+  {
+    "revision": "d78629d354bd0343555c",
+    "url": "/post-it/static/js/runtime-main.1c43c9fb.js"
+  }
+]);
